caadea6ff1791ac280fad476ae758f67ddbb5263
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-03-19 12:41:32 +0000

    End of stage 4

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,14 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
   it 'release a not broken bike' do
-    expect(subject.release_bike).not_to be_broken
+    subject.dock(double(:bike, broken?: false))
+    fake_bike = subject.release_bike
+    expect(fake_bike).not_to be_broken
+  end
+  it 'docks a bike' do
+    expect(subject.dock(:bike)).to eq :bike
+  end
+  it 'raises an error when no bikes avaialable' do
+    expect { subject.release_bike }.to raise_error 'No Bikes'
   end
 end
--- spec/feature/public_boris_bikes_spec.rb
+++ spec/feature/public_boris_bikes_spec.rb
@@ -4,7 +4,12 @@ require 'docking_station'
 feature 'Member of public can release a bike' do
   scenario 'Docking Station releases a not broken bike' do
     docking_station = DockingStation.new
+    docking_station.dock(Bike.new)
     bike = docking_station.release_bike
     expect(bike).not_to be_broken
+  end
+  scenario 'if the docking station is empty a bike is not released' do
+    docking_station = DockingStation.new
+    expect { docking_station.release_bike }.to raise_error 'No Bikes'
   end
 end
