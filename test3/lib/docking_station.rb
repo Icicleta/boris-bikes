a6f59054367b76c42374eefafa071f55ad55ed0d
icicleta <icicleta@gmail.com>
2015-03-19 20:10:01 +0000

    recover all tests

--- test3/lib/docking_station.rb
+++ test3/lib/docking_station.rb
@@ -1,11 +1,33 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_accessor :bike
-  alias_method :dock, :bike=
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @bikes = []
+    @capacity = capacity
+  end
 
   def release_bike
-    fail 'No Bikes' unless @bike
-    @bike
+    fail 'No Bikes' if empty?
+    bikes.pop
+  end
+
+  def dock(bike)
+    fail 'Station Full' if full?
+    bikes << bike
+    nil
+  end
+
+  private
+
+  attr_reader :bikes
+
+  def empty?
+    bikes.reject(&:broken?).length == 0
+  end
+
+  def full?
+    bikes.length >= DEFAULT_CAPACITY
   end
 end
